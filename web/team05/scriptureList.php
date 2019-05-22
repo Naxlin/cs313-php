016f6b10ca682e313927fd22fbc6949c9a73553f
naxlin <naxlin.everthaine@gmail.com>
2019-05-21 18:12:09 -0600

    test 3

--- web/team05/scriptureList.php
+++ web/team05/scriptureList.php
@@ -2,54 +2,53 @@
     $requestString = $_REQUEST['request'];
     $request = json_decode($requestString, true);
 
-    $commandMap = array("details"=>"details", "scripture"=>"scripture");
+    $commandMap = array('details'=>'details', 'scripture'=>'scripture');
     $commandMap[$request['cmd']]($request);
 
     function connect() {
-        try {
-            $dbUrl = getenv('DATABASE_URL');
+        // try {
+        //     $dbUrl = getenv('DATABASE_URL');
 
-            $dbOpts = parse_url($dbUrl);
+        //     $dbOpts = parse_url($dbUrl);
 
-            $dbHost = $dbOpts["host"];
-            $dbPort = $dbOpts["port"];
-            $dbUser = $dbOpts["user"];
-            $dbPassword = $dbOpts["pass"];
-            $dbName = ltrim($dbOpts["path"],'/');
+        //     $dbHost = $dbOpts['host'];
+        //     $dbPort = $dbOpts['port'];
+        //     $dbUser = $dbOpts['user'];
+        //     $dbPassword = $dbOpts['pass'];
+        //     $dbName = ltrim($dbOpts['path'],'/');
 
-            $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        //     $db = new PDO('pgsql:host=$dbHost;port=$dbPort;dbname=$dbName', $dbUser, $dbPassword);
 
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $db;
-        } catch (PDOException $ex) {
-            echo 'Error!: ' . $ex->getMessage();
-            die();
-        }
-
+        //     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //     return $db;
+        // } catch (PDOException $ex) {
+        //     echo 'Error!: ' . $ex->getMessage();
+        //     die();
+        // }
     }
 
-    // function details($obj) {
+    function details($obj) {
     //     $name = $obj['name'];
-    // }
+    }
 
     function scripture($obj) {
         // $book = $obj['book'];
         // $book = strtolower($book);    
-        // $list = "";
+        // $list = '';
         // $db = connect();    
-        // if ($book === "") {
+        // if ($book === ') {
         //     $sql = 'SELECT * FROM scriptures';
         //     $stmt = $db->prepare($sql);
         //     $stmt->execute();
         //     $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // } else if ($book !== "") {
+        // } else if ($book !== '') {
         //     $sql = 'SELECT (book, chapter, verse) FROM scriptures WHERE book LIKE :book';
         //     $stmt = $db->prepare($sql);            
         //     $stmt->bindParam(':book', '%' . $book . '%', PDO::PARAM_STR);
         //     $stmt->execute();
         //     $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
         // }
-        // echo $list === "" ? "No book with that name." : $list;
+        // echo $list === ' ? 'No book with that name.' : $list;
     }
 ?>
 
