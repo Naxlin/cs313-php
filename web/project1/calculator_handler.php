af1c950ae7b9c5d9e70e1cdc857498de8541d4bf
naxlin <naxlin.everthaine@gmail.com>
2019-05-25 23:41:14 -0600

    test

--- web/project1/calculator_handler.php
+++ web/project1/calculator_handler.php
@@ -64,9 +64,9 @@
                 $stmt->bindValue(':singularity_id', (int) $id, PDO::PARAM_INT);
                 $stmt->execute();
                 $p = $stmt->fetch(PDO::FETCH_ASSOC);
-                $help = str_replace(')', '', $p['row']);
-                $help = str_replace('(', '', $help);
-                $help = explode(',', $p);
+                $help = explode(',', $p['row']);
+                // $help = str_replace(')', '', $help);
+                // $help = str_replace('(', '', $help);
                 $comp_list = $comp_list . json_encode($help);
                 $comp_list = $comp_list . '<ul class="parents"><li class="parent">' . $l[$help[0]]['name'] . '</li>';
                 $comp_list = $comp_list . '<li class="parent">' . $l[$help[1]]['name'] . '</li>';
