8269bec806df288bedc6c08f88df438ff084641a
naxlin <naxlin.everthaine@gmail.com>
2019-05-25 19:59:02 -0600

    test

--- web/project1/calculator_handler.php
+++ web/project1/calculator_handler.php
@@ -33,7 +33,6 @@
         public $compound;
         public $item_cost;
         public $item;
-        public $parents = null;
     }
 
     function connect() {
@@ -71,15 +70,16 @@
         $stmt->execute();
         $singularities = $stmt->fetchAll(PDO::FETCH_CLASS, 'Singularity');
         foreach ($singularities as $row) {
+            echo $row;
             $sql = 'SELECT * FROM singularity_parents WHERE singularity = :singularity_id';
             $stmt = $db->prepare($sql);
-            $stmt->bindValue(':singularity_id', $row['singularity_id'], PDO::PARAM_INT);
+            $stmt->bindValue(':singularity_id', 1, PDO::PARAM_INT);
             $stmt->setFetchMode(PDO::FETCH_INTO, $parents);
             $stmt->execute();
             $parents = $stmt->fetchAll(PDO::FETCH_CLASS, "Parents");
-            $row['parents'] = $parents;
+            
         }
-        echo json_encode($singularities);
+        // echo json_encode($singularities);
     }
 
     function thaumcraft($obj) {
