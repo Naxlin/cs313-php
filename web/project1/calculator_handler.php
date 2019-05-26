3faf120067e6df200e1bcdbcc689d5cf789ab6dd
naxlin <naxlin.everthaine@gmail.com>
2019-05-25 19:37:03 -0600

    test

--- web/project1/calculator_handler.php
+++ web/project1/calculator_handler.php
@@ -7,6 +7,13 @@
     $commandMap = array('singularity'=>'singularity', 'thaumcraft'=>'thaumcraft', 'tinkers'=>'tinkers');
     $commandMap[$request['cmd']]($request);
 
+    class Rows
+    {
+        protected $cols;
+        function __set($name, $value) { $this->cols[$name] = $value; }
+        function __get($name) { return $this->cols[$name]; }
+    }
+
     class Parents {
         public $parent1;
         public $parent2;
@@ -17,6 +24,14 @@
         public $parent7;
         public $parent8;
         public $parent9;
+    }
+
+    class Singularity {
+        public $sing_id;
+        public $sing_name;
+        public $sing_comp;
+        public $sing_cost;
+        public $sing_item;
     }
 
     function connect() {
@@ -44,32 +59,24 @@
 
     function singularity($obj) {
         $name = $obj['name'];
-        $list = '';
+        $parents = new Rows();
+        $singularities = new Rows();
         $comp_list = '';
         $db = connect();
         $sql = 'SELECT (singularity_id, singularity_name, compound, item_cost, item) FROM singularities WHERE singularity_name LIKE :name';
         $stmt = $db->prepare($sql);
         $stmt->bindValue(':name', "%$name%", PDO::PARAM_STR);
+        $stmt->setFetchMode(PDO::FETCH_INTO, $singularities);
         $stmt->execute();
-        $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($list as $row => $item) {
-            $items = explode(',', $item['row']);
-            $id = substr($items[0], 1);
-            $comp_list = $comp_list . $id . ' ' . $items[1] . ' ' . $items[2] . ' ' . $items[3] . ' ' . substr($items[4], 0, -1) . ' ';
-
-            if ($items[2] == t) {
-                $sql = 'SELECT (parent1, parent2, parent3, parent4, parent5, parent6, parent7, parent8, parent9) FROM singularity_parents WHERE singularity = :singularity_id';
-                $stm = $db->prepare($sql);
-                $stm->bindValue(':singularity_id', $id, PDO::PARAM_INT);
-                $stm->setFetchMode(PDO::FETCH_CLASS, 'Parents');
-                $stm->execute();
-                $parents = $stm->fetch();
-                echo $parents;
-            }
-        }
-        
-        // $compiled_list = $compiled_list . '</ul>';
-        echo "singularity - " . $comp_list;
+        $singularities = $stmt->fetchAll(PDO::FETCH_CLASS, 'Singularity');
+        $sql = 'SELECT (parent1, parent2, parent3, parent4, parent5, parent6, parent7, parent8, parent9) FROM singularity_parents WHERE singularity = :singularity_id';
+        $stm = $db->prepare($sql);
+        $stm->bindValue(':singularity_id', $id, PDO::PARAM_INT);
+        $stm->setFetchMode(PDO::FETCH_INTO, $parents);
+        $stm->execute();
+        $tools = $stmt->fetchAll(PDO::FETCH_CLASS, "Parents");
+        $parents = $stm->fetch();
+        echo $parents;
     }
 
     function thaumcraft($obj) {
