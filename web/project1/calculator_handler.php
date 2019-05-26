ee19c03183f8aa22f430539b240d1bfe56ba9582
naxlin <naxlin.everthaine@gmail.com>
2019-05-25 22:01:49 -0600

    test

--- web/project1/calculator_handler.php
+++ web/project1/calculator_handler.php
@@ -55,7 +55,7 @@
                 $comp_list = $comp_list . '<ul class="parents">';
                 $sql = 'SELECT (parent1, parent2, parent3, parent4, parent5, parent6, parent7, parent8, parent9) FROM singularity_parents WHERE singularity = :singularity_id';
                 $stmt = $db->prepare($sql);
-                $stmt->bindValue(':singularity_id', $id, PDO::PARAM_INT);
+                $stmt->bindValue(':singularity_id', (int) $id, PDO::PARAM_INT);
                 $stmt->execute();
                 $parents = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 foreach ($parents as $a => $li) {
