8e3cae9e453010940fe8136e0efd561c3ccca396
naxlin <naxlin.everthaine@gmail.com>
2019-05-25 20:03:16 -0600

    test

--- web/project1/calculator_handler.php
+++ web/project1/calculator_handler.php
@@ -69,17 +69,17 @@
         $stmt->setFetchMode(PDO::FETCH_INTO, $singularities);
         $stmt->execute();
         $singularities = $stmt->fetchAll(PDO::FETCH_CLASS, 'Singularity');
-        // foreach ($singularities as $row) {
-            // echo $row;
-            $sql = 'SELECT * FROM singularity_parents WHERE singularity = :singularity_id';
-            $stmt = $db->prepare($sql);
-            $stmt->bindValue(':singularity_id', 1, PDO::PARAM_INT);
-            $stmt->setFetchMode(PDO::FETCH_INTO, $parents);
-            $stmt->execute();
-            $parents = $stmt->fetchAll(PDO::FETCH_CLASS, "Parents");
+        foreach ($singularities as $row) {
+            echo $row;
+            // $sql = 'SELECT * FROM singularity_parents WHERE singularity = :singularity_id';
+            // $stmt = $db->prepare($sql);
+            // $stmt->bindValue(':singularity_id', 1, PDO::PARAM_INT);
+            // $stmt->setFetchMode(PDO::FETCH_INTO, $parents);
+            // $stmt->execute();
+            // $parents = $stmt->fetchAll(PDO::FETCH_CLASS, "Parents");
             
-        // }
-        echo $singularities;
+        }
+        // echo $singularities;
     }
 
     function thaumcraft($obj) {
