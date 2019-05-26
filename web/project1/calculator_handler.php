42bd0c8b78f2cac7628952d31eb80fd2f2ca4a0e
naxlin <naxlin.everthaine@gmail.com>
2019-05-25 22:04:52 -0600

    test

--- web/project1/calculator_handler.php
+++ web/project1/calculator_handler.php
@@ -58,9 +58,9 @@
                 $stmt->bindValue(':singularity_id', (int) $id, PDO::PARAM_INT);
                 $stmt->execute();
                 $parents = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                foreach ($parents as $a) {
+                foreach ($parents as $a => $li) {
                     // $stuffs = explode(',', $li['row']);
-                    $comp_list = $comp_list . '<li class="parent">' . $a . '</li>';
+                    $comp_list = $comp_list . '<li class="parent">' . $li . '</li>';
                     // $comp_list = $comp_list . '<li class="parent">' . substring($stuffs[0], 1) . '</li>';
                     // $comp_list = $comp_list . '<li class="parent">' . $stuffs[1] . '</li>';
                     // $comp_list = $comp_list . '<li class="parent">' . $stuffs[2] . '</li>';
