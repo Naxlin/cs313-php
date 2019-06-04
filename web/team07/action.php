b52e3521c01d49d429e8044795de36071fa84769
naxlin <naxlin.everthaine@gmail.com>
2019-06-04 16:48:17 -0600

    team07 without a team

--- web/team07/action.php
+++ web/team07/action.php
@@ -3,7 +3,7 @@
 session_start();
 require("connect.php");
 
-$user = $_POST['user'];
+$username = $_POST['user'];
 $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
 
 $db = connect();
@@ -11,7 +11,7 @@ $db = connect();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$sql = 'INSERT INTO credentials (username, password) VALUES (:user, :pass)';
 	$stmt = $db->prepare($sql);
-	$stmt->bindValue(':user', $user, PDO::PARAM_STR);
+	$stmt->bindValue(':user', $username, PDO::PARAM_STR);
 	$stmt->bindValue(':pass', $pass, PDO::PARAM_STR);
 	$stmt->execute();
 	header("Location:login.php");
@@ -20,9 +20,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 	$sql = 'SELECT username, password FROM credentials WHERE username = :user';
 	$stmt = $db->prepare($sql);
-	$stmt->bindValue(':user', $user, PDO::PARAM_STR);
+	$stmt->bindValue(':user', $username, PDO::PARAM_STR);
 	$stmt->execute();
 	$row = $stmt->fetch();
+	var_dump($_POST);
 	var_dump($row);
 	if (array_key_exists('username', $row)) {
 		if (password_verify($_POST['pass'], $row['password'])) {
