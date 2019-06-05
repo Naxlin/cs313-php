4075fb99c4b1541f33db92abd213faeae1b48497
naxlin <naxlin.everthaine@gmail.com>
2019-06-04 19:01:56 -0600

    team07 alone

--- web/team07/action.php
+++ web/team07/action.php
@@ -3,13 +3,8 @@
 session_start();
 require("connect.php");
 $_SESSION['match'] = 'true';
-
 $username = $_POST['user'];
 $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
-var_dump($_POST['pass']);
-var_dump($_POST['passC']);
-
-
 $db = connect();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -17,13 +12,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		if (strcmp($_POST['pass'], $_POST['passC']) != 0) {
 			$_SESSION['match'] = 'false';
 			header("Location:signup.php");
+		} else {
+			$sql = 'INSERT INTO credentials (username, password) VALUES (:user, :pass)';
+			$stmt = $db->prepare($sql);
+			$stmt->bindValue(':user', $username, PDO::PARAM_STR);
+			$stmt->bindValue(':pass', $pass, PDO::PARAM_STR);
+			$stmt->execute();
+			header("Location:login.php");
 		}
-		$sql = 'INSERT INTO credentials (username, password) VALUES (:user, :pass)';
-		$stmt = $db->prepare($sql);
-		$stmt->bindValue(':user', $username, PDO::PARAM_STR);
-		$stmt->bindValue(':pass', $pass, PDO::PARAM_STR);
-		$stmt->execute();
-		// header("Location:login.php");
 	} else if ($_POST['check'] == 'true') {
 		$sql = 'SELECT username, password FROM credentials WHERE username = :user';
 		$stmt = $db->prepare($sql);
@@ -34,10 +30,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			if (password_verify($_POST['pass'], $row['password'])) {
 				$_SESSION['logged in'] = true;
 				$_SESSION['user'] = $row['username'];
-				// header("Location:welcome.php");
+				header("Location:welcome.php");
 			}
 		} else {
-			// header("Location:signup.php");
+			header("Location:signup.php");
 		}
 	}
 } 
