fe10371d48ca8ee31ffc9f151a2ed6e2028dda40
naxlin <naxlin.everthaine@gmail.com>
2019-06-04 18:42:25 -0600

    team07 without a team

--- web/team07/action.php
+++ web/team07/action.php
@@ -8,21 +8,22 @@ $username = $_POST['user'];
 $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
 var_dump($_POST['pass']);
 var_dump($_POST['passC']);
-if (strcmp($_POST['pass'], $_POST['passC']) != 0) {
-	$_SESSION['match'] = 'false';
-	header("Location:signup.php");
-}
+
 
 $db = connect();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if ($_POST['check'] == 'false') {
+		if (strcmp($_POST['pass'], $_POST['passC']) != 0) {
+			$_SESSION['match'] = 'false';
+			header("Location:signup.php");
+		}
 		$sql = 'INSERT INTO credentials (username, password) VALUES (:user, :pass)';
 		$stmt = $db->prepare($sql);
 		$stmt->bindValue(':user', $username, PDO::PARAM_STR);
 		$stmt->bindValue(':pass', $pass, PDO::PARAM_STR);
 		$stmt->execute();
-		header("Location:login.php");
+		// header("Location:login.php");
 	} else if ($_POST['check'] == 'true') {
 		$sql = 'SELECT username, password FROM credentials WHERE username = :user';
 		$stmt = $db->prepare($sql);
